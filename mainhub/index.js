1da3119fb0248ab053b943d6ec97070464fa0c27
Josh Holmes <josh.holmes@microsoft.com>
2015-02-12 11:39:54 -0800

    trying to pull wunderground info

--- mainhub/index.js
+++ mainhub/index.js
@@ -1,4 +1,4 @@
-//require('./wunderground.js');
+require('./wunderground.js');
 var sensorReadings = require('../common/sensorReadings.js');
 
 var mraa = require("mraa"); // A new object of class "mraa"
@@ -88,6 +88,46 @@ function updateLocal() {
 	listOfStuffToDisplay["local"].temperature = getTemp();
 	listOfStuffToDisplay["local"].light = getLight();
 }
+
+function updateWeatherUnderground() {
+	var myJson = require("./hourly.json");
+	// a quick tool showing how to read the stored hourly forecasts
+
+	// compare with current time
+	var epoch = (new Date).getTime() / 1000;
+
+	for (var i in myJson) {
+	  var forecast = myJson[i];
+	  var forecastEpoch = forecast.FCTTIME.epoch;
+	  var futureDiff = forecastEpoch - epoch
+	  var futureHour = Math.floor(futureDiff / 3600);
+	  if (futureHour < 0 ) {
+	    // if forecasts are in the past ignore them
+	    continue;
+	  }
+
+	  var readings = {};
+	  readings.temperature = forecast.temp.english;
+	  readings.wind = forecast.wspd.english;
+	  readings.baro = forecast.mslp.english;
+
+	  listOfStuffToDisplay["w"] = readings;
+
+	  // not sure what you want to do with these data - just logging them out here
+	  console.log( 'in ' + futureHour + ' hours'
+	    + ' temp: ' + forecast.temp.english
+	    + ' wind: ' + forecast.wspd.english
+	    + ' baro: ' + forecast.mslp.english
+	    + ' humid: ' + forecast.humidity
+	  );
+	}
+}
+
+setTimeout(function{
+	setInterval(function() {
+		updateWeatherUnderground();
+	}, 10000);
+}, 10000);
 
 var B = 3975;
 function getTemp()
