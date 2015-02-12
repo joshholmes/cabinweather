78db774fbcaa034d94abf4fb6ab7767519824458
Josh Holmes <josh.holmes@microsoft.com>
2015-02-12 10:06:57 -0800

    fixed getDateTime()

--- mainhub/index.js
+++ mainhub/index.js
@@ -35,7 +35,7 @@ console.log('Server running *:1337/');
 
 setInterval(function() {
 	myLcd.setCursor(0,0);
-	myLcd.write(fixedLengthString(getDateTime())); // doesn't work for some reason
+	myLcd.write(fixedLengthString(getDateTime())); 
 	displayReadings();
 },1000);
 
@@ -76,3 +76,27 @@ function getLight()
 	return a; // return the light
 }
 
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return hour + ":" + min + ":" + sec + " " + year + ":" + month + "/" + day;
+}
