5f445397c9d7065be2c93e009ad1cf2344a0a4c5
Josh Holmes <josh.holmes@microsoft.com>
2015-02-11 22:45:30 -0800

    added a quick test of the screen

--- mainhub/index.js
+++ mainhub/index.js
@@ -1,3 +1,15 @@
 //require('./wunderground.js');
 
+//var LCD = require('jsupm_i2clcd');
+
+// Load i2clcd module
 var LCD = require('jsupm_i2clcd');
+//Initialize Jhd1313m1 at 0x62 (RGB_ADDRESS) and 0x3E (LCD_ADDRESS) 
+var myLcd = new LCD.Jhd1313m1(0, 0x3E, 0x62);
+myLcd.setColor(64,255,64);
+setTimeout(function() {
+	console.log("Attempting to say hi");
+	myLcd.setCursor(0,0);
+	myLcd.write('Hello World'); // doesn't work for some reason
+	console.log("Done saying hi");
+},1000);
