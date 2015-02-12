b1a8f3a251f106863acf38bc41226622efcd268b
Josh Holmes <josh.holmes@microsoft.com>
2015-02-11 22:53:40 -0800

    added a quick test lcd with clock

--- mainhub/index.js
+++ mainhub/index.js
@@ -1,15 +1,41 @@
 //require('./wunderground.js');
 
-//var LCD = require('jsupm_i2clcd');
+var LCD = require('jsupm_i2clcd');
 
 // Load i2clcd module
 var LCD = require('jsupm_i2clcd');
+
+var date = new Date();
 //Initialize Jhd1313m1 at 0x62 (RGB_ADDRESS) and 0x3E (LCD_ADDRESS) 
 var myLcd = new LCD.Jhd1313m1(0, 0x3E, 0x62);
 myLcd.setColor(64,255,64);
-setTimeout(function() {
-	console.log("Attempting to say hi");
+setInterval(function() {
 	myLcd.setCursor(0,0);
-	myLcd.write('Hello World'); // doesn't work for some reason
-	console.log("Done saying hi");
+	myLcd.write('Hello World: ' + getDateTime(); // doesn't work for some reason
 },1000);
+
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
