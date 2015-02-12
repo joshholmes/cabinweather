d62130acc5afc73338e273ac4ccba1a381e9f7ae
Josh Holmes <josh.holmes@microsoft.com>
2015-02-11 23:58:16 -0800

    adding a little web service

--- mainhub/index.js
+++ mainhub/index.js
@@ -13,6 +13,15 @@ var date = new Date();
 var myLcd = new LCD.Jhd1313m1(0, 0x3E, 0x62);
 myLcd.setColor(64,255,64);
 
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
+
+
 setInterval(function() {
 	myLcd.setCursor(0,0);
 	myLcd.write(fixedLengthString(getDateTime())); // doesn't work for some reason
